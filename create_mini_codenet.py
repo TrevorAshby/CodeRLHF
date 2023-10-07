54f3b91edc2484712ef4d26934e5a7dd10d376bc
Fausto German <faustotnc@gmail.com>
2023-10-07 01:32:40 -0400

    Mini CodeNet
    Added a new function to create a mini version of the CodeNet project (25% of the C++, C, and Python submissions).
    
    I generated a static tar.gz file for mini_codenet, and can grant access to the file on request.
    
    Closes #3

--- create_mini_codenet.py
+++ create_mini_codenet.py
@@ -1,31 +1,68 @@
 import os
 import shutil
+from tqdm import tqdm
+import pandas as pd
 
-def create_mini(path_to_files, mini_perc):
-    path_to_mini = "./mini_codenet/data/"
-    for prob in os.listdir(path_to_files):
-        # create problem folder
-        os.mkdir(path_to_mini + prob)
-        for lang in os.listdir(path_to_files + prob + "/"):
-            # create language folder
-            os.mkdir(path_to_mini + prob + "/" + lang)
-            ten_perc = int(len(os.listdir(path_to_files + prob + "/" + lang))*mini_perc)
-            idx = 0
-            for file_name in os.listdir(path_to_files + prob + "/" + lang):
-                # copy file over
-                src = path_to_files + prob + "/" + lang + "/" + file_name
-                dst = path_to_mini + prob + "/" + lang + "/" + file_name
-                shutil.copyfile(src, dst)
 
-                idx += 1
-                if idx == ten_perc:
-                    break
-            
+def create_mini(src_folder, tgt_folder, languages, mini_percent, random_state=None):
+    src_path_to_meta = src_folder + "metadata"
+    src_path_to_data = src_folder + "data"
+
+    # For each metadata file...
+    for meta in tqdm(os.listdir(src_path_to_meta)):
+        if meta == "problem_list.csv":
+            shutil.copyfile(src_path_to_meta+"/"+meta, tgt_folder+meta)
+            continue
+
+        if meta.startswith("p") and meta.endswith(".csv"):
+            # Step #1 – Take metadata files and select rows with desired languages.
+            meta_df = pd.read_csv(src_path_to_meta + "/" + meta)
+            meta_df = meta_df[meta_df["language"].isin(languages)]
+
+            # If there are no submissions made in the desired language, then ignore the problem.
+            if len(meta_df) == 0:
+                continue
+
+            # Step #2 – Remove unneeded columns from the new DataFrame
+            meta_df.drop(
+                columns=["user_id", "date", "original_language"],
+                inplace=True
+            )
+
+            # Step #3 – Random sample while keeping distribution of submission status
+            meta_df = meta_df.groupby(["status"]).sample(
+                frac=mini_percent,
+                replace=False,
+                random_state=random_state
+            )
+
+            # Step #4 – Read submission files in random sample
+            submissions_contents = []
+            columns = ["problem_id", "language",
+                       "submission_id", "filename_ext"]
+            for problem_id, lang, submission_id, file_ext in meta_df[columns].values:
+                submission_path = f"{src_path_to_data}/{problem_id}/{lang}/{submission_id}.{file_ext}"
+                with open(submission_path, "r") as submission_contents:
+                    submissions_contents.append(submission_contents.read())
+
+            # Step #5 – Append submission contents to the dataframe
+            meta_df["solution"] = submissions_contents
+
+            # Step #6 – Save dataframe as feather file
+            meta_df.reset_index(inplace=True)
+            meta_df.to_feather(tgt_folder + meta[:-4] + ".ftr")
+
 
 if __name__ == '__main__':
-    mini_percent = 0.1 # percentage of the original dataset that mini should consist of | MODIFY-ABLE
+    # percentage of the original dataset that mini should consist of | MODIFY-ABLE
+    mini_percent = 0.25
     if mini_percent <= 1.0:
-        create_mini('./Project_CodeNet/data/', mini_percent)
+        create_mini(
+            src_folder='./Project_CodeNet/',
+            tgt_folder="./mini_codenet/data/",
+            languages=["C++", "C", "Python"],
+            mini_percent=mini_percent
+        )
         print('Finished creating mini_codenet')
     else:
         print('ERROR: MAKE SURE THAT mini_percent IS <= 1.0')
